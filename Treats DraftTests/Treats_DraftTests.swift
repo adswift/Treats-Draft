80785347a7a7564b0889f23d9f39d1f43f399ddb
Adeena <adeena.ansari@gmail.com>
2020-02-14 19:44:03 +0800

    Initial Commit

---
+++ Treats DraftTests/Treats_DraftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Treats_DraftTests.swift
+//  Treats DraftTests
+//
+//  Created by Adeena Ansari on 14/2/20.
+//  Copyright © 2020 Adeena Ansari. All rights reserved.
+//
+
+import XCTest
+@testable import Treats_Draft
+
+class Treats_DraftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
